84093f95c1b9a6f27d7585b7d586e14becbe1463
clitetailor <ducnhatphung@gmail.com>
2018-10-17 08:26:37 +0700

    Resolve the unexpected behavior with shareReplay

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,8 +1,5 @@
 import { of } from 'rxjs'
-import { delay, map, reduce } from 'rxjs/operators'
-import { syncReplay } from 'rxjs-sync-operator'
-
-import { overlapMap } from '../index'
+import { delay, map, reduce, share } from 'rxjs/operators'
 
 const reduceAll = () =>
   reduce((acc, value) => acc.concat([value]), [])
@@ -25,49 +22,13 @@ test('should output 1, 5', done => {
     )
 })
 
-test('should show unexpected behavior', done => {
-  const origin = Date.now()
-
-  //    v           v  v
-  of(3, 1, 5, 6, 7, 3, 5)
-    .pipe(
-      map(val => of(val).pipe(delay(val * 100))),
-      overlapMap(stream => stream),
-      map(value => ({
-        time: Math.round((Date.now() - origin) / 100),
-        value
-      })),
-      reduceAll()
-    )
-    .subscribe(
-      values => {
-        expect(values).toEqual([
-          {
-            time: 2, // takeUntil 1s + delay 1s
-            value: 1
-          },
-          // {
-          //   time: 6, <- takeUntil 5s, being omitted
-          //   value: 3
-          // },
-          {
-            time: 10, // takeUntil 5s + delay 5s
-            value: 5
-          }
-        ])
-      },
-      reportError,
-      done
-    )
-})
-
 test('should execute in parallel', done => {
   const origin = Date.now()
 
   of(3, 1, 5, 6, 7, 3, 8, 9, 5)
     .pipe(
       map(val => of(val).pipe(delay(val * 100))),
-      overlapMap(syncReplay()),
+      overlapMap(stream => stream),
       map(value => ({
         time: Math.round((Date.now() - origin) / 100),
         value
--- index.js
+++ index.js
@@ -3,22 +3,25 @@ import {
   scan,
   switchMap,
   takeUntil,
-  concat
+  concat,
+  shareReplay
 } from 'rxjs/operators'
 
 export function overlap(...sources) {
-  return sources.reduce((acc, next) =>
-    acc.pipe(
-      takeUntil(next),
-      concat(next)
+  return sources.reduce((acc, next) => {
+    const shared = next.pipe(share())
+    return acc.pipe(
+      takeUntil(shared),
+      concat(shared)
     )
-  )
+  })
 }
 
 export function overlapMap(callback) {
   return source =>
     source.pipe(
       map(callback),
+      map(shareReplay()),
       scan((prev, curr) =>
         prev.pipe(
           takeUntil(curr),
